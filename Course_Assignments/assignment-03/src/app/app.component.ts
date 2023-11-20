123f878e2a6b02931825271be8af36729710289b
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2023-11-20 21:46:10 +0530

    completed new assignment-03

--- Course_Assignments/assignment-03/src/app/app.component.ts
+++ Course_Assignments/assignment-03/src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'assignment-03';
+  showSecret = false;
+  clickLog = [];
+
+  listClasses = [
+    'list-group-item-primary',
+    'list-group-item-secondary',
+    'list-group-item-success',
+    'list-group-item-danger',
+    'list-group-item-warning',
+    'list-group-item-info',
+    'list-group-item-light',
+    'list-group-item-dark',
+  ];
+
+  toggleDisplay(): void {
+    this.showSecret = !this.showSecret;
+    this.clickLog.push(new Date().toISOString());
+  }
+
+  returnClass(index: number): string {
+    if (index > 7) {
+      const result = this.listClasses[1];
+      return result;
+    } else {
+      return this.listClasses[index];
+    }
+  }
 }
