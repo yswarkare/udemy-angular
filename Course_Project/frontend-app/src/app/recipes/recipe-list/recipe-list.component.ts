44203c783ecc6a0c9adcde3fcf53521df1270549
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2023-11-21 21:39:56 +0530

    course project update

--- Course_Project/frontend-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course_Project/frontend-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a simple test recipe',
+      'https://www.cookwithmanali.com/wp-content/uploads/2017/03/Best-Kadai-Paneer.jpg'
+    ),
+    new Recipe(
+      'A test recipe 2',
+      'This is a simple test recipe',
+      'https://www.cookwithmanali.com/wp-content/uploads/2017/03/Best-Kadai-Paneer.jpg'
+    ),
+  ];
 }
--- Course_Project/frontend-app/src/app/shopping-list/shopping-list.component.ts
+++ Course_Project/frontend-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [new Ingredient('', '')];
 }
