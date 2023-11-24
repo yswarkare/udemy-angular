f602c3d6e300ee6d251423f15e53495fb28c7210
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2023-11-24 14:15:40 +0530

    added list of recipes

---
+++ packages/data/recipes.data.js
@@ -0,0 +1,92 @@
+const recipes = [
+{
+	name: 'Restaurant Style Garlic Naan',
+	description: 'Soft and delicious this is the only Garlic Naan recipe that you will ever need! Make restaurant style garlic naan at home with this easy step-by-step recipe.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2018/09/Garlic-Naan.jpg',
+	url: 'https://www.cookwithmanali.com/restaurant-style-garlic-naan/'
+},
+{
+	name: 'Aloo Kulcha',
+	description: 'Indian cuisine has a huge range of breads but the only one people outside India are aware of is naan. And naan is the least likely bread to be made in an average Indian household, oh what an irony! The more popular everyday breads are roti and paratha, which are made with whole wheat. That is what we usually eat with our sabzi (spiced veggies) and dal (lentils).',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2015/08/Aloo-Kulcha.jpg',
+	url: "https://www.cookwithmanali.com/aloo-kulcha/"
+},
+{
+	name: 'Paneer Paratha',
+	description: 'Paneer Paratha is a popular whole wheat Indian flatbread. It’s filled with crumbled paneer (Indian cottage cheese) and makes a filling breakfast or lunch.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2013/09/Paneer-Paratha-Homemade.jpg',
+	url: 'https://www.cookwithmanali.com/paneer-paratha/',
+},
+{
+	name: 'Aloo Paratha',
+	description: 'Indian Aloo Paratha – whole wheat pan-fried flatbread stuffed with a spicy potato filling. This paratha is best enjoyed with yogurt, pickle, butter and a side of chai! Aloo paratha is a whole wheat flatbread which is stuffed with spicy mashed potatoes. It is pan fried with oil or ghee and is usually enjoyed with butter, yogurt or pickle.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2017/01/Aloo-Paratha-Indian.jpg',
+	url: 'https://www.cookwithmanali.com/aloo-paratha/',
+},
+{
+	name: 'Pav Bhaji',
+	description: 'Pav Bhaji is popular Indian street food where dinner rolls/buns are served with spicy mashed veggies topped with dollop of butter. Street food doesn’t get better than this! This Pav Bhaji Recipe is spicy, so flavorful and can be easily made vegan by replacing butter with vegan butter. If you have never had this in your life, you are seriously missing out!',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2018/05/Best-Pav-Bhaji-Recipe-800x1212.jpg',
+	url: 'https://www.cookwithmanali.com/pav-bhaji/',
+},
+{
+	name: 'Papdi Chaat',
+	description: 'Papdi Chaat is a popular street food from North India where crispy papdi (deep fried flour crackers) are topped with a variety of toppings like chickpeas, boiled potato, yogurt, chutneys, cilantro and more! These are highly addictive and are like a burst of flavor in every bite!',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2022/03/Papdi-Chaat-676x1024.jpg',
+	url: 'https://www.cookwithmanali.com/papdi-chaat/',
+},
+{
+	name: 'Chilli Cheese Toast',
+	description: 'Chilli Cheese Toast is a popular snack and street food in India especially in Mumbai. It has slices of bread topped with cheese (usually Amul cheese) and green chilies for that kick. Veggies like onions, green bell peppers, and green onions are also often added to it. This step by step recipe will guide you on how to make chilli cheese toast on a tawa/skillet (stove top) and also in an air fryer.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2023/06/Chilli-Cheese-Toast-800x1212.jpg',
+	url: 'https://www.cookwithmanali.com/chilli-cheese-toast/',
+},
+{
+	name: 'Paneer Butter Masala (Paneer Makhani)',
+	description: 'Creamy Restaurant style Paneer Butter Masala is easy to make at home. This creamy curry is made with onion, tomatoes, cashews, cream and butter (of course!). t has a mild sweet taste and pairs beautifully with garlic naan or jeera rice.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2019/05/Paneer-Butter-Masala.jpg',
+	url: 'https://www.cookwithmanali.com/paneer-butter-masala-recipe/',
+},
+{
+	name: 'Paneer Tikka Naan Pizza',
+	description: 'Paneer Tikka Naan Pizza- a super flavorful Indian style vegetarian pizza made using paneer, spices, cheese and homemade naan. This pizza is perfect for a date night or whenever the pizza craving kicks in!',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2021/02/Paneer-Tikka-Naan-Pizza-Recipe.jpg',
+	url: 'https://www.cookwithmanali.com/paneer-tikka-naan-pizza/',
+},
+{
+	name: 'Matar Paneer',
+	description: 'Make rich and creamy Matar Paneer at home with this step by step recipe! Matar Paneer is one of the most popular paneer recipes where paneer (Indian cottage cheese) and matar (green peas) are simmered together in a luscious gravy made with tomatoes, onions and spices.',
+	imagePath: 'https://www.cookwithmanali.com/wp-content/uploads/2023/06/Easy-Matar-Paneer-800x1212.jpg',
+	url: 'https://www.cookwithmanali.com/matar-paneer/',
+},
+{
+	name: '',
+	description: '',
+	imagePath: '',
+	url: '',
+},
+{
+	name: '',
+	description: '',
+	imagePath: '',
+	url: '',
+},
+{
+	name: '',
+	description: '',
+	imagePath: '',
+	url: '',
+},
+{
+	name: '',
+	description: '',
+	imagePath: '',
+	url: '',
+},
+{
+	name: '',
+	description: '',
+	imagePath: '',
+	url: '',
+},
+]
