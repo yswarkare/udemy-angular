12f7a0a161a0752307d8091ee36ddb3c3a218708
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-04-07 15:14:36 +0530

    completed observables practice

--- course-practice/observables-practice-app/src/app/app.component.ts
+++ course-practice/observables-practice-app/src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated: boolean = false;
+  activatedSubscription: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    // this.userService.activatedEmitter.subscribe((didActivated: boolean) => {
+    //   this.userActivated = didActivated;
+    // });
+
+    this.activatedSubscription = this.userService.activatedSubject.subscribe(
+      (didActivated: boolean) => {
+        this.userActivated = didActivated;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activatedSubscription.unsubscribe();
+  }
 }
