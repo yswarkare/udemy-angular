c14d552989c0fdbfa80f22c3d68079a3f75c3af3
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2024-04-08 18:02:53 +0530

    completed forms template driven assignment

--- Course_Assignments/assignment-06-forms/src/app/app.component.ts
+++ Course_Assignments/assignment-06-forms/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'assignment-06-forms';
+
+  @ViewChild('signupForm') signupForm!: NgForm;
+
+  userData = {
+    email: '',
+    password: '',
+    subscription: '',
+  };
+
+  formSubmitted = false;
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.formSubmitted = true;
+    this.userData.email = this.signupForm.value.email;
+    this.userData.password = this.signupForm.value.password;
+    this.userData.subscription = this.signupForm.value.subscription;
+    this.signupForm.resetForm();
+  }
 }
